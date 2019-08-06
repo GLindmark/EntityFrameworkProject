3b2ef239a03685497c2a36761d3ae0f3792a0ec8
lindmark421@gmail.com <lindmark421@gmail.com>
2019-08-06 13:42:57 -0400

    Added Context

---
+++ EntityFrameworkProject/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkProject.Models {
+    //should have the word 'Context' in the class to remind you of what it is
+    public class AppDbContext : DbContext {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)//options should be plural.  The constructor must be the same name as class.
+            : base(options) {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ EntityFrameworkProject/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkProject.Models {
+    public class Student {
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int SAT { get; set; }
+        public double GPA { get; set; }
+        public bool IsFulltime { get; set; }
+        //must have a public default constructor
+        public Student() {
+
+        }
+
+    }
+}
