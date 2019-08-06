acc631a7b6cdf74511be31ba32db54494bf861f4
lindmark421@gmail.com <lindmark421@gmail.com>
2019-08-06 14:53:59 -0400

    Created Tables

--- EntityFrameworkProject/Models/AppDbContext.cs
+++ EntityFrameworkProject/Models/AppDbContext.cs
@@ -5,11 +5,14 @@ using System.Text;
 
 namespace EntityFrameworkProject.Models {
     //should have the word 'Context' in the class to remind you of what it is
+
     public class AppDbContext : DbContext {
 
-        public AppDbContext(DbContextOptions<AppDbContext> options)//options should be plural.  The constructor must be the same name as class.
-            : base(options) {
+        public AppDbContext() : base() { }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+            var connStr = "server=localhost\\sqlexpress;database=AppEfdB;trusted_connection=true";
+            builder.UseSqlServer(connStr);
         }
 
         public DbSet<Student> Students { get; set; }
--- EntityFrameworkProject/Models/Student.cs
+++ EntityFrameworkProject/Models/Student.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EntityFrameworkProject.Models {
     public class Student {
 
         public int Id { get; set; }
+        [Required]//makes Firstname not null
+        [StringLength(30)]//limits the characters to 30
         public string Firstname { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Lastname { get; set; }
+        [Range(600, 2400, ErrorMessage = "SAT score 600 <=x,=2400")]
         public int SAT { get; set; }
         public double GPA { get; set; }
-        public bool IsFulltime { get; set; }
+        public bool IsFulltime { get; set; } = true;
         //must have a public default constructor
         public Student() {
 
